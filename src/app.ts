9fbca96ace7d6c36910e67f14f58fa79e40984cf
GLeonidasG <48949253+GLeonidasG@users.noreply.github.com>
2020-08-13 14:58:07 -0300

    Added swagger prototype doc

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import { Route } from './routes';
+import swaggerDocument from './swagger.json';
 class App {
     app: express.Application;
 
@@ -13,6 +15,7 @@ class App {
     middleware() {
         this.app.use( cors() );
         this.app.use( express.json() );
+        this.app.use( '/swagger', swaggerUi.serve ,swaggerUi.setup( swaggerDocument ) );
     }
 
     routes() {
